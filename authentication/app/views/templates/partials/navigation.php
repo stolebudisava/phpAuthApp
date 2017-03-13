23594a742c76fb2a38f38cd5dbc1f2c9b77477a0
stojan-mitric-devtech <stojan.mitric.contractor@ensighten.com>
2017-03-13 16:02:06 +0100

    Added profile view for user and list view of all users

--- authentication/app/views/templates/partials/navigation.php
+++ authentication/app/views/templates/partials/navigation.php
@@ -1,15 +1,14 @@
-{% if auth %}
-<p>Hello, {{ auth.getFullNameOrUsername() }}! </p>
-<img src="{{ auth.getAvatarUrl( {size: 30} ) }}" alt="Your avatar"> </img>
-{% endif %}
 
 <ul>
-	    <li> <a href="{{ urlFor('home') }}"> Home </a> </li>
+    <li> <a href="{{ urlFor('home') }}"> Home </a> </li>
 
     {% if auth %}
         <li> <a href="{{ urlFor('logout') }}"> Log out </a> </li>
+        <li> <a href="{{ urlFor('user.profile', {username: auth.username}) }}"> Your profile </a> </li>
     {% else %}
         <li> <a href="{{ urlFor('register') }}"> Register </a> </li>
         <li> <a href="{{ urlFor('login') }}"> Login </a> </li>
     {% endif %}
+        <li> <a href="{{ urlFor('user.all') }}"> All users </a> </li>
+
 </ul>
